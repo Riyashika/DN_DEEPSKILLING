77273c5952bd67ba3fc27cee87cc11eba253c4fa
Riyashika <riyashika866@gmail.com>
2026-06-23 14:37:23 +0530

    Update MyServiceTest.java

--- Week_1/Ex1_MockingStubbing/MyServiceTest.java
+++ Week_1/Ex1_MockingStubbing/MyServiceTest.java
@@ -6,16 +6,10 @@ public class MyServiceTest {
 
     @Test
     public void testExternalApi() {
-        // 1. Create mock
         ExternalApi mockApi = mock(ExternalApi.class);
-
-        // 2. Stub the method
         when(mockApi.getData()).thenReturn("Mock Data");
-
-        // 3. Use the mock in the service under test
         MyService service = new MyService(mockApi);
         String result = service.fetchData();
-
         assertEquals("Mock Data", result);
     }
 }
