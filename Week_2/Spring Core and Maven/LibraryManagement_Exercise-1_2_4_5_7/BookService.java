6f37d8719a4f65e0c5570d6c1bdbd8d5ea1eb8e9
Riyashika <riyashika866@gmail.com>
2026-06-23 15:07:50 +0530

    Add BookService class for managing book details

---
+++ Week_2/Spring Core and Maven/LibraryManagement_Exercise-1_2_4_5_7/BookService.java
@@ -0,0 +1,16 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+public class BookService {
+private BookRepository bookRepository;
+public BookService() {
+}
+public BookService(BookRepository bookRepository) {
+this.bookRepository = bookRepository;
+}
+public void setBookRepository(BookRepository bookRepository) {
+this.bookRepository = bookRepository;
+}
+public String getBookDetails() {
+return bookRepository.findBook();
+}
+}
