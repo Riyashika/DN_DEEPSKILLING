986fe26fef56673407f3394a2ab923f682790094
Riyashika <riyashika866@gmail.com>
2026-07-11 23:23:19 +0530

    Week 4: Account/Loan microservices, Eureka discovery server, API Gateway with global logging filter

---
+++ Week_4/Create Eureka Discovery Server and register/eureka-discovery-server/src/test/java/com/cognizant/eurekadiscoveryserver/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.cognizant.eurekadiscoveryserver;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class EurekaDiscoveryServerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
